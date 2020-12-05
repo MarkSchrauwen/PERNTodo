01140c8fc888e1e5eb6c2ee6bdbab847ab1e094d
MarkSchrauwen <schrauwen.mark@gmail.com>
2020-12-05 19:41:15 +0100

    client-folder has to be added

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json()); //req.body;
 
- if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     // server static content is made by npm run build inside client-folder
-    app.use(express.static(path.join(__dirname,"client/build")));
+    app.use(express.static(path.join(__dirname, "client/build")));
 }
 
 // Routes
