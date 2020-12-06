8a6e09af0689e76cfa3afc0e413eaabc3ef1179a
MarkSchrauwen <schrauwen.mark@gmail.com>
2020-12-06 10:32:19 +0100

    ; toegevoegd aan SQL-statements

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 app.post('/todos',async(req,res) => {
     try {
         const {description} = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *", 
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES ($1) RETURNING *;", 
         [description]);
         res.json(newTodo.rows[0]);
     } catch (err) {
@@ -34,7 +34,7 @@ app.post('/todos',async(req,res) => {
 // Get all todos
 app.get('/todos',async(req,res) => {
     try {
-        const allTodos = await pool.query('SELECT * FROM todo');
+        const allTodos = await pool.query('SELECT * FROM todo;');
         res.json(allTodos.rows);
     } catch (err) {
         console.error(err.message);
@@ -45,7 +45,7 @@ app.get('/todos',async(req,res) => {
 app.get('/todos/:id',async(req,res) => {
     try {
         const {id} = req.params;
-        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1;", [id]);
         res.json(todo.rows[0]);
     } catch (err) {
         console.error(err.message);
@@ -57,7 +57,7 @@ app.put('/todos/:id',async(req,res) => {
     try {
         const {id} = req.params;
         const {description} = req.body;
-        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2;",
         [description, id]);
         res.json("Todo was updated !");
     } catch (err) {
@@ -69,7 +69,7 @@ app.put('/todos/:id',async(req,res) => {
 app.delete('/todos/:id', async(req,res) => {
     try {
         const {id} = req.params;
-        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1;", [id]);
         res.json('Todo was successfully deleted !');
     } catch (err) {
         console.log(err.message);
